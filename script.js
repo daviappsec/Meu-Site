ad10940867e0bf334e2d1e220e146bde76174a70
daviconceicaoconceicao01-netizen <daviconceicaoconceicao01@gmail.com>
2026-07-26 00:42:24 -0300

    atualizações no css

--- script.js
+++
@@ -1,6 +0,0 @@
-const botao = document.getElementById("menu-btn");
-const menu = document.getElementById("menu");
-
-botao.addEventListener("click", () => {
-    menu.classList.toggle("ativo");
-});
